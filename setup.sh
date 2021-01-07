e0dfe98bdeeac4384d3766e451fa11ce89cb1299
BUPTNJ <122296743@qq.com>
2021-01-07 09:58:37 +0800

    1

---
+++ setup.sh
@@ -0,0 +1,6 @@
+wget https://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20200923.tar.gz
+tar -xzvf scamper-cvs-20200923.tar.gz
+cd scamper-cvs-20200923
+./configure
+make
+make install
