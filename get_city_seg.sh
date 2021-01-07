6bf098b0e2cc131f0efc49373c736533bab2c7c6
BUPTNJ <122296743@qq.com>
2021-01-07 16:43:39 +0800

    1

--- get_city_seg.sh
+++ get_city_seg.sh
@@ -1,3 +1,9 @@
+
+#judge if href_list exist
+if [ ! -e "href_list" ]; then
+    ./get_href_list.sh
+fi
+
 city_name=$1
 echo "extracting href...">&2
 href=`cat href_list | grep $city_name |gawk '{print $2}'`
@@ -6,4 +12,6 @@ wget $href -O $file_name
 echo "formatting html file...">&2
 ./format.sh $file_name
 cat $file_name |gawk -f parse_seg
+echo "cleaning html..."
+rm $file_name
 echo "done!">&2
