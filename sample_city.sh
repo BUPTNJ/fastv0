334e8c750882d182aaa27a555eac66061383d445
BUPTNJ <122296743@qq.com>
2021-01-14 10:20:57 +0800

    1

--- sample_city.sh
+++ sample_city.sh
@@ -1,6 +1,6 @@
 #judge if city_seg exist
 file_name="$1_seg"
 if [ ! -e $file_name ]; then
-    ./get_city_seg.sh $1 > $file_name
+    ./get_city_seg.sh $1 > ./seg/$file_name
 fi
 cat $file_name | gawk -f sample_target
